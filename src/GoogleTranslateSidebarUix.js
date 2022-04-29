dba92558be5415086fae27dfe07a3e8dc472b0a3
Cadu de Castro Alves <cadudecastroalves@gmail.com>
2022-04-29 18:22:56 +0200

    Translate multiple languages at once, add support to Rich Text Editor

--- src/GoogleTranslateSidebarUix.js
+++ src/GoogleTranslateSidebarUix.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import {
     Wrapper,
     useUiExtension,
@@ -5,76 +7,138 @@ import {
 
 import axios from 'axios';
 
+import { Button, Stack } from '@mui/material';
+import GTranslateIcon from '@mui/icons-material/GTranslate';
+import { astToHtmlString } from '@graphcms/rich-text-html-renderer';
+import htmlToSlateAST from '@graphcms/html-to-slate-ast';
+
+const getLanguageCode = (languageCode) => {
+    if (languageCode.startsWith('localization_')) {
+        return languageCode.slice(-2);
+    } else {
+        return languageCode.substring(0, 2);
+    }
+}
+
 const GoogleTranslateWidget = () => {
-    const { allLocales, extension, form: { getState, change } } = useUiExtension();
+    const { allLocales, extension, form: { getFieldState, change, subscribeToFormState } } = useUiExtension();
+    const [translatableFields, setTranslatableFields] = useState([]);
+    const [buttonLabel, setButtonLabel] = useState('Translate');
 
     const authKey = extension.config.API_KEY;
 
-    const getLanguageCode = (languageCode) => {
-        return languageCode.substring(0, 2);
-    }
-
     const sourceLanguage = getLanguageCode(allLocales[0].apiId);
-    const targetLanguage = getLanguageCode(allLocales[1].apiId);
-    const defaultLocalization = 'localization_' + sourceLanguage;
-    const targetLocalization = 'localization_' + targetLanguage;
+    const defaultLanguage = 'localization_' + sourceLanguage;
+
+    useEffect(() => {
+
+        const getTranslatableFields = (formFields) => {
+            const fields = Object.entries(formFields.modified);
+            const translatableFields = [];
+            const defaultFields = [];
+
+            fields.map((field) => {
+                let fieldKey = field[0];
+                if (fieldKey.startsWith('localization_')) {
+                    if (fieldKey.startsWith(defaultLanguage)) {
+                        defaultFields.push(fieldKey);
+                    } else {
+                        translatableFields.push(fieldKey);
+                    }
+                }
+                return true;
+            });
+
+            return {
+                defaultFields,
+                translatableFields,
+            };
+        }
+
+        let unsubscribe;
+        subscribeToFormState(
+            (state) => {
+                const { defaultFields, translatableFields } = getTranslatableFields(state);
+                // console.log('translatable:', { defaultFields, translatableFields })
+                setTranslatableFields(translatableFields);
+            },
+            {
+                modified: true
+            }
+        ).then((formState) => unsubscribe = formState);
+
+        return () => {
+            unsubscribe?.();
+        }
+    }, [subscribeToFormState, defaultLanguage]);
 
     const translate = () => {
 
-        getState().then(({values}) => {
+        setButtonLabel('Translating...');
 
-            const asArray = Object.entries(values);
-            const filtered = asArray.filter(([key]) => key.startsWith('localization_'));
-            const translatableFields = Object.fromEntries(filtered);
+        // console.log('translatableFields:', translatableFields);
+        // console.log('defaultLanguageFields:', defaultLanguageFields);
 
-            // console.log('translatableFields:', translatableFields);
+        translatableFields.map((field) => {
+            const [fieldPrefix, fieldApiId] = field.split('.');
+            const targetLanguage = getLanguageCode(fieldPrefix);
 
-            for(let field in translatableFields[defaultLocalization]) {
-            
-                if(field !== 'updatedAt') {
-                    const currentText = translatableFields[defaultLocalization][field];
-                    axios.post(
-                        'https://translation.googleapis.com/language/translate/v2?key=' + authKey, 
-                        {
-                            q: currentText,
-                            source: sourceLanguage,
-                            target: targetLanguage,
-                            format: 'text',
-                        }
-                    ).then((response) => {
-                        if(response?.data?.data?.translations) {
-                            let newText = response?.data?.data?.translations[0].translatedText;
-                            change(`${targetLocalization}.${field}`, newText);
-                        }
-                    }).catch((error) => {
-                        console.log(error);
-                    });
+            const defaultLanguageField = defaultLanguage + '.' + fieldApiId;
+            getFieldState(defaultLanguageField).then(({ value }) => {
+
+                let textToTranslate = value;
+
+                // Rich Text Editor - Transform AST to HTML
+                if(fieldApiId === 'contentRte') {
+                    const content = textToTranslate.raw;
+                    textToTranslate = astToHtmlString({content});
+                    console.log('html:', textToTranslate);
                 }
 
-            }
+                axios.post(
+                    'https://translation.googleapis.com/language/translate/v2?key=' + authKey,
+                    {
+                        q: textToTranslate,
+                        source: sourceLanguage,
+                        target: targetLanguage,
+                        format: 'text',
+                    }
+                ).then((response) => {
+                    if (response?.data?.data?.translations) {
+                        let newText = response?.data?.data?.translations[0].translatedText;
+
+                        // RTE - HTML to AST
+                        if(fieldApiId === 'contentRte') {
+                            // console.log('html ' + targetLanguage + ':', newText);
+                            htmlToSlateAST(newText).then((astValue) => {
+                                const content = {
+                                    raw: {
+                                        children: astValue
+                                    }
+                                };
+                                // console.log(`ast ${targetLanguage}:`, content);
+                                change(`${fieldPrefix}.${fieldApiId}`, content);
+                            });
+                        } else {
+                            change(`${fieldPrefix}.${fieldApiId}`, newText);
+                        }
+                    }
+                }).catch((error) => {
+                    console.log(error);
+                });
+
+                setButtonLabel('Translate');
+
+            });
+
+            return true;
         });
     }
 
-    const btnStyle = {
-        alignItems: 'center',
-        backgroundColor: '#32c48d',
-        border: '0',
-        borderRadius: '4px',
-        color: '#ffffff',
-        display: 'inline-flex',
-        fontWeight: '500px',
-        height: '32px',
-        lineHeight: '16px',
-        marginBottom: '8px',
-        padding: '1px 12px',
-        textAlign: 'center',
-        verticalAlign: 'middle',
-    };
-
     return (
-        <>
-            <button style={btnStyle} onClick={translate}>Translate to {allLocales[1].displayName}</button><br />
-        </>
+        <Stack>
+            <Button startIcon={<GTranslateIcon />} variant='contained' size='small' onClick={translate} disableElevation>{buttonLabel}</Button>
+        </Stack>
     );
 };
 
